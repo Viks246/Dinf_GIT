b74b89f4a51381e2c5e01856faf5aff4e5f0f3ca
Victoria Vega Navarro <victoria.v.n@hotmail.com>
2023-10-08 20:35:27 +0200

    Primera version

---
+++ src/main/java/com/mycompany/primeraaplicacion/PrimeraAplicacion.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.primeraaplicacion;
+
+/**
+ *
+ * @author victo
+ */
+public class PrimeraAplicacion {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
